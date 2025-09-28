335fc212589ccdd6d26f487fc924786ec590da0f
alqof <alqofnuraji@gmail.com>
2025-09-29 00:31:20 +0700

    layout header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,19 +4,22 @@ import NavItems from "@/components/NavItems";
 // import {searchStocks} from "@/lib/actions/finnhub.actions";
 import UserDropdown from "./UseDropdown";
 
-const Header = async ({ user }: { user: User }) => {
+// const Header = async ({ user }: { user: User }) => {
+const Header = () => {
     // const initialStocks = await searchStocks();
 
     return (
         <header className="sticky top-0 header">
             <div className="container header-wrapper">
                 <Link href="/">
-                    <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
+                    <Image src="/assets/icons/qopstock-logo.svg" alt="Qopztock-logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
                 </Link>
                 <nav className="hidden sm:block">
+                    <NavItems />
                     {/* <NavItems initialStocks={initialStocks} /> */}
                 </nav>
 
+                <UserDropdown />
                 {/* <UserDropdown user={user} initialStocks={initialStocks} /> */}
             </div>
         </header>
