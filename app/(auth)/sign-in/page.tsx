a58b77a2e3d9b6d9d241330117a0f42ef9c75a9b
alqof <alqofnuraji@gmail.com>
2025-10-08 18:01:01 +0700

    update

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -46,7 +46,7 @@ const SignIn = () => {
                 <InputField
                     name="email"
                     label="Email"
-                    placeholder="contact@jsmastery.com"
+                    placeholder="user@mail.com"
                     register={register}
                     error={errors.email}
                     validation={{ required: 'Email is required', pattern: /^\w+@\w+\.\w+$/ }}
