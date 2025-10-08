56b2322d6c786267fc3d6a13ce7dd6ad2963ca4a
alqof <alqofnuraji@gmail.com>
2025-10-08 17:37:34 +0700

    stock detail

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -27,13 +27,14 @@ export const sendSignUpEmail = inngest.createFunction(
                         parts: [
                             { text: prompt }
                         ]
-                    }]
+                    }
+                ]
             }
         })
 
         await step.run('send-welcome-email', async () => {
             const part = response.candidates?.[0]?.content?.parts?.[0];
-            const introText = (part && 'text' in part ? part.text : null) ||'Thanks for joining Signalist. You now have the tools to track markets and make smarter moves.'
+            const introText = (part && 'text' in part ? part.text : null) || 'Thanks for joining Signalist. You now have the tools to track markets and make smarter moves.'
 
             const { data: { email, name } } = event;
 
